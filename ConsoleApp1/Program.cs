7f0fedff5f11da1fc8544f8ce829768d8e7f55af
NurMuhammadRidlo1 <nurmuhammadridlo@gmail.com>
2022-03-18 10:48:56 +0700

    Melengkapi fitur input, Menambahkan method perhitungan, dan Menambahkan pengkondisian untuk pilihan input user

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,8 @@ namespace Kalkulator
         {
             Console.Title = "Aplikasi Kalkulator";
 
+            int hasil = 0;
+
             Console.WriteLine("Pilih Menu Kalkulator: \n");
             Console.WriteLine("1. Penambahan");
             Console.WriteLine("2. Pengurangan");
@@ -16,7 +18,53 @@ namespace Kalkulator
 
             Console.Write("Masukan Menu Pilihan : ");
             int pilihan = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nMasukan angka Pertama : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Masukan angka Kedua   : ");
+            int b = Convert.ToInt32(Console.ReadLine());
 
+            if( pilihan == 1 )
+            {
+                hasil = Penambahan(a, b);
+            } else if( pilihan == 2)
+            {
+                hasil = Pengurangan(a, b);
+            } else if( pilihan == 3)
+            {
+                hasil = Perkalian(a, b);
+            } else if(pilihan == 4)
+            {
+                hasil = Pembagian(a, b);
+            } else
+            {
+                Console.WriteLine("Pilihan yang anda masukan tidak tersedia!");
+            }
+
+            Console.Write("\nHasil dari perhitungan adalah : ");
+            Console.Write(hasil + "\n");
+
+        }
+        static int Penambahan (int a, int b)
+        {
+            int hasil = a + b;
+            return hasil;
+        }
+
+        static int Pengurangan (int a, int b)
+        {
+            int hasil = a - b;
+            return hasil;
+        }
+     
+        static int Perkalian (int a, int b)
+        {
+            int hasil = a * b;
+            return hasil;
+        }
+        static int Pembagian(int a, int b)
+        {
+            int hasil = a / b;
+            return hasil;
         }
     }
 }
